975261ebcae7ba3df3c61e0f40ccb8989f62389a
Barrett Anderson <barrett1614@gmail.com>
2024-09-24 09:58:32 -0500

    Added FecthColor.JS/Removed unneeded files

---
+++ src/fetchColor.js
@@ -0,0 +1,22 @@
+async function fetchColorData() {
+  try {
+    const response = await fetch("http://localhost:3000/color");
+    const data = await response.json();
+    updateCSS(data.color);
+  } catch (error) {
+    console.error("Error fetching color data:", error);
+  }
+}
+
+function updateCSS(color) {
+  const styleSheet = document.styleSheets[0];
+  const keyframes = `
+      @keyframes changeColor {
+        0% { background: radial-gradient(circle at 20% 20%, #ffffff, ${color}); }
+        100% { background: radial-gradient(circle at 30% 30%, #ffffff, ${color}); }
+      }
+    `;
+  styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+}
+
+document.addEventListener("DOMContentLoaded", fetchColorData);
